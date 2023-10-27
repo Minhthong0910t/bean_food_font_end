6fb06a6ccdbd4c74328060150dd041528a934e45
huan <dinhhuan645@gmail.com>
2023-10-27 10:17:20 +0700

    user

--- profile/ProfileInfor.js
+++ profile/ProfileInfor.js
@@ -1,72 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, Image, Alert } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, Image, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
 
-// Default avatar image URL
-const defaultAvatarUrl = 'https://tse2.mm.bing.net/th?id=OIP.U096knBymS0Oz-4PAvxH7AHaHa&pid=Api&P=0&h=180';
-
-const ProfileInfor = () => {
-    const [username, setUsername] = useState('');
-    const [userData, setUserData] = useState({
-        sdt: '',
-        avatar: '',
-        gender: '',
-        birthdate: '',
-    });
-    const [isEditing, setIsEditing] = useState(false);
-    const navigation = useNavigation();
+export default function UpdateProfile() {
+    const [phone, setPhone] = useState('');
+    const [avatar, setAvatar] = useState('');
+    const [gender, setGender] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [isProfileLoaded, setIsProfileLoaded] = useState(false);
 
     useEffect(() => {
-        const checkLoginStatus = async () => {
-            try {
-                const token = await AsyncStorage.getItem('username');
-                const token2 = await AsyncStorage.getItem('userId');
-                if (token) {
-                    setUsername(token);
-                    fetchUserInfo(token);
-                    console.log("username   "+ token);
-                    console.log("username   "+ token2);
-                } else {
-                    navigation.replace('Login');
-                }
-            } catch (error) {
-                console.error('Lỗi khi kiểm tra trạng thái đăng nhập:', error);
-            }
-        };
-
-        checkLoginStatus();
+        // Lấy thông tin người dùng từ AsyncStorage nếu đã tồn tại
+        getStoredUserData();
     }, []);
 
-    const fetchUserInfo = (token) => {
-        fetch(`http://example.com/api/user-info?token=${token}`)
-            .then(async (res) => {
-                if (res.status === 200) {
-                    const userInfo = await res.json();
-                    setUsername(userInfo.username);
-                    setUserData({
-                        sdt: userInfo.sdt || '',
-                        avatar: userInfo.avatar || '', // Use the user's avatar or the default
-                        gender: userInfo.gender || '',
-                        birthdate: userInfo.birthdate || '',
-                    });
-                }
-            })
-            .catch((error) => {
-                console.error('Lỗi khi lấy thông tin người dùng:', error);
-            });
+    const getStoredUserData = async () => {
+        try {
+            const storedPhone = await AsyncStorage.getItem('phone');
+            const storedAvatar = await AsyncStorage.getItem('avatar');
+            const storedGender = await AsyncStorage.getItem('gender');
+            const storedBirthday = await AsyncStorage.getItem('birthday');
+
+            if (storedPhone) {
+                setPhone(storedPhone);
+            }
+            if (storedAvatar) {
+                setAvatar(storedAvatar);
+            }
+            if (storedGender) {
+                setGender(storedGender);
+            }
+            if (storedBirthday) {
+                setBirthday(storedBirthday);
+            }
+
+            setIsProfileLoaded(true);
+        } catch (error) {
+            console.error('Lỗi khi lấy thông tin từ AsyncStorage:', error);
+        }
     };
 
-    const handleUpdateUserData = async () => {
+    const handleUpdateProfile = async () => {
         try {
             const token = await AsyncStorage.getItem('username');
+            const userId = await AsyncStorage.getItem('userId');
+
             const updateData = {
-                sdt: userData.sdt,
-                avatar: userData.avatar,
-                gender: userData.gender,
-                birthdate: userData.birthdate,
+                phone,
+                avatar,
+                gender,
+                birthday,
             };
-            const response = await fetch('http://234324234/api/updateUserData', {
+
+            // cập nhật thông tin người dùng
+            const response = await fetch(`http://192.168.1.8:3000/api/users/update/${userId}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -76,90 +63,60 @@ const ProfileInfor = () => {
             });
 
             if (response.status === 200) {
-                setIsEditing(false);
-                Alert.alert('Thông báo', 'Cập nhật thành công');
+                Alert.alert('Thông báo', 'Cập nhật thông tin thành công');
             } else {
                 Alert.alert('Lỗi', 'Cập nhật thất bại');
             }
         } catch (error) {
-            console.error('Lỗi khi cập nhật', error);
+            console.error('Lỗi khi cập nhật:', error);
         }
     };
 
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Image
-                    source={{ uri: userData.avatar || defaultAvatarUrl }}
-                    style={styles.avatar}
-                />
-                <View style={styles.info}>
-                    <Text style={styles.usernameText}>Tên đăng nhập: {username}</Text>
-                    {isEditing && (
-                        <TextInput
-                            placeholder="Avatar URL"
-                            value={userData.avatar}
-                            onChangeText={(text) => setUserData({ ...userData, avatar: text })}
-                            style={styles.input}
-                            editable={isEditing}
-                        />
-                    )}
+            {isProfileLoaded ? (
+                <>
                     <TextInput
                         placeholder="Số điện thoại"
-                        value={userData.sdt}
-                        onChangeText={(text) => setUserData({ ...userData, sdt: text })}
+                        value={phone}
+                        onChangeText={(text) => setPhone(text)}
                         style={styles.input}
-                        editable={isEditing}
                     />
                     <TextInput
-                        placeholder="Giới tính"
-                        value={userData.gender}
-                        onChangeText={(text) => setUserData({ ...userData, gender: text })}
+                        placeholder="Avatar URL"
+                        value={avatar}
+                        onChangeText={(text) => setAvatar(text)}
                         style={styles.input}
-                        editable={isEditing}
+                    />
+                    {avatar ? <Image source={{ uri: avatar }} style={styles.avatarImage} /> : null}
+                    <TextInput
+                        placeholder="Giới tính"
+                        value={gender}
+                        onChangeText={(text) => setGender(text)}
+                        style={styles.input}
                     />
                     <TextInput
                         placeholder="Ngày sinh"
-                        value={userData.birthdate}
-                        onChangeText={(text) => setUserData({ ...userData, birthdate: text })}
+                        value={birthday}
+                        onChangeText={(text) => setBirthday(text)}
                         style={styles.input}
-                        editable={isEditing}
                     />
-                </View>
-                <View style={styles.buttonEdit}>
-                    {isEditing ? (
-                        <Button title="Lưu" onPress={handleUpdateUserData} style={styles.btn} />
-                    ) : (
-                        <Button title="Chỉnh sửa thông tin" onPress={() => setIsEditing(true)} style={styles.btn} />
-                    )}
-                </View>
-            </View>
+                    <Button title="Cập nhật thông tin" onPress={handleUpdateProfile} style={styles.btn} />
+                </>
+            ) : (
+                <Text>Đang tải thông tin...</Text>
+            )}
         </View>
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 70,
         flex: 1,
-        alignItems: 'flex-start',
-    },
-    header: {
-        flexDirection: 'row',
-        marginLeft: 10,
-    },
-    avatar: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-        margin: 10,
-    },
-    info: {
-        flexDirection: 'column',
-        marginLeft: 10,
+        alignItems: 'center',
+        padding: 20,
     },
     input: {
-        marginTop: 10,
         width: '100%',
         height: 40,
         borderColor: 'gray',
@@ -168,19 +125,13 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 15,
     },
-    buttonEdit: {
-        marginTop: 20,
-        marginLeft: 40,
-        borderRadius: 10,
-    },
-    usernameText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'gray',
+    avatarImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        margin: 10,
     },
     btn: {
         backgroundColor: '#319AB4',
     },
 });
-
-export default ProfileInfor;
